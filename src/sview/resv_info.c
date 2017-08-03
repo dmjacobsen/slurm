dmjacobsen/slurm

commit 22c9e25cae29dda3f9823b262506f83faf311591

Improve error output for updating reservations.

src/sview/resv_info.c
@@ -321,12 +321,14 @@ static const char *_set_resv_msg(resv_desc_msg_t *resv_msg,
 				 int column)
 {
 	char *type = "", *temp_str;
-	char *tmp_text, *last = NULL, *tok, *core_cnt = NULL;
+	char *tmp_text, *last = NULL, *tok;
 	int block_inx, temp_int = 0;
 	uint32_t f;
 
 	/* need to clear global_edit_error here (just in case) */
 	global_edit_error = 0;
+	if (global_edit_error_msg)
+		g_free(global_edit_error_msg);
 
 	if (!resv_msg)
 		return NULL;
@@ -348,13 +350,16 @@ static const char *_set_resv_msg(resv_desc_msg_t *resv_msg,
 		type = "burst_buffer";
 		break;
         case SORTID_CORE_CNT:
-                type = "core count";
-		core_cnt = xstrdup(new_text);
-                if (_parse_resv_core_cnt(resv_msg, core_cnt) == SLURM_ERROR) {
-			xfree(core_cnt);
+		if (cluster_flags & CLUSTER_FLAG_BG)
+			type = "Cnode Count";
+		else
+			type = "Core Count";
+                if (_parse_resv_core_cnt(resv_msg, new_text) == SLURM_ERROR) {
+			global_edit_error_msg = g_strdup_printf(
+				"Can't use %s when system "
+				"is not running cons_res select plugin", type);
 			goto return_error;
                 }
-		xfree(core_cnt);
                 break;
 	case SORTID_DURATION:
 		temp_int = time_str2mins((char *)new_text);
@@ -502,9 +507,7 @@ static gboolean _admin_focus_out_resv(GtkEntry *entry,
 		const char *name = gtk_entry_get_text(entry);
 		type -= DEFAULT_ENTRY_LENGTH;
 		col_name = _set_resv_msg(resv_msg, name, type);
-		if (global_edit_error) {
-			if (global_edit_error_msg)
-				g_free(global_edit_error_msg);
+		if (global_edit_error && !global_edit_error_msg) {
 			global_edit_error_msg = g_strdup_printf(
 				"Reservation %s %s can't be set to %s",
 				resv_msg->name,
@@ -1709,7 +1712,13 @@ static void _admin_resv(GtkTreeModel *model, GtkTreeIter *iter, char *type)
 			if (got_edit_signal)
 				goto end_it;
 
-			if (!global_send_update_msg) {
+			if (global_edit_error) {
+				temp = g_strdup_printf(
+					"Something was wrong with the "
+					"values you wanted to change: %s",
+					global_edit_error_msg ?
+					global_edit_error_msg : "unknown");
+			} else if (!global_send_update_msg) {
 				temp = g_strdup_printf("No change detected.");
 			} else if (slurm_update_reservation(resv_msg)
 				   == SLURM_SUCCESS) {

commit c7c4c002b5910938e028fe233b94f623077f91aa

sview - fix SIGABRT when incorrectly freeing global_edit_error_msg.
Bug was introduced in 22c9e25cae29. Stack trace:

Thread 1 (Thread 0x7ff64228c300 (LWP 6652)):
../sysdeps/unix/sysv/linux/raise.c:58
fmt=fmt@entry=0x7ff6409bb000 "*** Error in `%s': %s: 0x%s ***\n") at
../sysdeps/posix/libc_fatal.c:175
ptr=<optimized out>, str=0x7ff6409bb110 "double free or corruption (out)",
 action=3) at malloc.c:5048
 have_lock=<optimized out>) at malloc.c:3904
 malloc.c:2984
 new_text=0x561a4f8b4d10 "asfasdf", column=21) at
 ../../../../slurm/src/sview/resv_info.c:331
 event=0x561a4f8a8f10, resv_msg=0x561a4f8b1410) at
 ../../../../slurm/src/sview/resv_info.c:515

Bug 2329

src/sview/resv_info.c
@@ -327,8 +327,6 @@ static const char *_set_resv_msg(resv_desc_msg_t *resv_msg,
 
 	/* need to clear global_edit_error here (just in case) */
 	global_edit_error = 0;
-	if (global_edit_error_msg)
-		g_free(global_edit_error_msg);
 
 	if (!resv_msg)
 		return NULL;