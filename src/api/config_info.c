6d4bc351596b92bcab1f0eeaa8e08233a00575b4
Doug Jacobsen <dmjacobsen@lbl.gov>
2017-10-01 11:43:38 -0700

    get server side reboots working

--- src/api/config_info.c
+++ src/api/config_info.c
@@ -1319,6 +1319,14 @@ extern void *slurm_ctl_conf_2_key_pairs (slurm_ctl_conf_t* slurm_ctl_conf_ptr)
 	list_append(ret_list, key_pair);
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("RebootNodesServerSide");
+	if (slurm_ctl_conf_ptr->reboot_nodes_serverside)
+		key_pair->value = xstrdup("Yes");
+	else
+		key_pair->value = xstrdup("No");
+	list_append(ret_list, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
 	key_pair->name = xstrdup("RebootProgram");
 	key_pair->value = xstrdup(slurm_ctl_conf_ptr->reboot_program);
 	list_append(ret_list, key_pair);
